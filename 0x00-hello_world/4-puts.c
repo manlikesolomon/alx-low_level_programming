6c00735e67b81d3d944510e9ed47774e588e8ae8
Solomon <balogun97@icloud.com>
2022-10-14 18:06:46 -0700

    C program that prints a string via puts -Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
  * Description: Program prints a string followed by a new line
  * Return: int 0 when there are no errors
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }	
