eb861382d064f06b3a5b7536008dedc8f548ca2b
Solomon <balogun97@icloud.com>
2022-10-14 18:04:10 -0700

    C program that prints a string via puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a string via puts
+ * Description: Program prints a string followed by a new line
+ * Return: int 0 when there are no errors
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}	
