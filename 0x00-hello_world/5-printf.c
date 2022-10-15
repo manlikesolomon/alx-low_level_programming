34729d202ab7392cf00ad6aa0530c47f955fe96a
Solomon <balogun97@icloud.com>
2022-10-14 18:20:54 -0700

    C program that prints a string via printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a string via printf
+ * Description: Program taht prints out a string followed by a new line
+ * Return: int 0 when run with no errors
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
