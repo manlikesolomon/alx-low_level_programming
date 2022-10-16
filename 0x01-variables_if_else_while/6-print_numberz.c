0a0f88eaca9e3e11b6c0f16b0bd7e1795b1e0033
Solomon <balogun97@icloud.com>
2022-10-16 14:46:39 -0700

    Task 6 - printed numbers in base 10 using putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers in base 10
+ * Return: Returns 0 for success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
