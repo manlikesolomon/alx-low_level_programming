71c1239d8b33d375e2ac71efdfea9356aeb1d679
Solomon <balogun97@icloud.com>
2022-10-16 15:53:28 -0700

    Task 7 - Printing the alphabet backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets backwards
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; a--)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
