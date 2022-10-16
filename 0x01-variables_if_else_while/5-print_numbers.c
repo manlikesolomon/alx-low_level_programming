2285b186839d77dbd2b9a5e5e37219622eea5191
Solomon <balogun97@icloud.com>
2022-10-16 14:32:54 -0700

    Task 5 -Printed numbers from zero to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 10
+ * Description: fuction prints base 10 numbers (0 to 10)
+ * Return: Returns 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
