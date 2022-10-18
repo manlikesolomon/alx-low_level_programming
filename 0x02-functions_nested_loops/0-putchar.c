cf4f6a1fb36cb33cb83aa7c1ff1dd8a081be75bf
Solomon <balogun97@icloud.com>
2022-10-18 05:45:55 -0700

    a program that prints _putchar, followed by a new line (3rd attempt)

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <std.io>
 #include "main.h"
 /**
  * main - prints the string _putchar
+ * Description: standard libraries are not used
  * Return: returns zero (0) for success
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char string[] = "_putchar";
+	int i = 0;
+
+	while (string[i] != '\0')
+	{
+		_putchar(string[i]);
+		i++;
+	}
+	_putchar('\n')
 	return (0);
 }
