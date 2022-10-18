4b85f92f418d68473a954e40d54314e0e91be889
Solomon <balogun97@icloud.com>
2022-10-18 12:04:54 -0700

    function adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - sums two integers
+ * @x: int type number
+ * @y: int type number
+ * Return: sum of numbers
+ */
+
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
