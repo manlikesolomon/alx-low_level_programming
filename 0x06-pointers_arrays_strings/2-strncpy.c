126adfd829a9b25609509fa97f886f2719cde961
Solomon <balogun97@icloud.com>
2022-10-27 09:26:49 -0700

    a function that copies a (string 2nd attempt)

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n && src[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 
 	for (; i < n; i++)
 	{
-		dest += '\0';
+		dest[i] += '\0';
 	}
 
 	return (dest);
