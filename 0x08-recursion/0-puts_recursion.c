d73ee2c06f4e337799e12b3d48e219fcc2ffd895
Solomon <balogun97@icloud.com>
2022-11-02 17:53:59 -0700

    Recursive function that prints all chars in a string (4th attempt)

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,7 +13,7 @@ void _puts_recursion(char *s)
 	}
 	else
 	{
-		_putchar(*s);
+		_putchar(s[0]);
 		_put_recursion(s + 1);
 	}
 }
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,1 @@
 This directory contains c scripts written to practice recursive functions.
-
