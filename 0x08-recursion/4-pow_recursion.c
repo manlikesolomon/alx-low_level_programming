abd6e9c528fef91cb1a1f4e0c94d2a65f0803152
Solomon <balogun97@icloud.com>
2022-11-02 18:59:18 -0700

    raises x to power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - reaises x to power y
+ * @x: argumant to raise
+ * @y: power
+ * Return: raised number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+	{
+		return (x);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
