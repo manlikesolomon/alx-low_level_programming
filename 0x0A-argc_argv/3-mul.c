a692867e2b27b5e591f7a818b8ed53174d7bc7af
Solomon <balogun97@icloud.com>
2022-11-04 15:41:24 -0700

    a program that multiplies two numbers and prints the result

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,10 +14,11 @@ int main(int argc, char *argv[])
 	if (argc < 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
 	else
 	{
-		mul = atio(argv[1]) * atio(argv[2]);
+		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
 	}
 	return (0);
