6e846593754669c3879e1f413161cd226b904dec
Solomon <balogun97@icloud.com>
2022-11-04 15:05:08 -0700

    fuction that prints the name of the script

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the function
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 for success
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains c scripts to practice working with argc and argv in the main functions.
