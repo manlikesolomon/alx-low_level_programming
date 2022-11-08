53cdbf1742a6d467985916d11945283351659003
Solomon <balogun97@icloud.com>
2022-11-07 21:29:11 -0800

    function that creates an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - creates an array
+ * @size: size of array
+ * @c: char to start from
+ * Return: char type c
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		char *arr = (char *)malloc(sizeof(char) * size);
+
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = c;
+		}
+		return (arr);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains c scripts used to test dynamic memory allocation.
