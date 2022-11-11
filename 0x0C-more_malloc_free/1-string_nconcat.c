5807a95e5eaa55af14b653e88fab010922e9da46
Solomon <balogun97@icloud.com>
2022-11-11 07:07:45 -0800

    concats two strings using malloc function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != '\0')
+	for (i = 0; s1[i] != '\0'; i++)
 		len++;
 	concat = malloc(sizeof(char) * (len + 1));
 
