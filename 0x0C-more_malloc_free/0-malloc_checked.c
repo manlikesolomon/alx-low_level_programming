fb2166646c0f4f1a73158edfcd64b5bec59f560b
Solomon <balogun97@icloud.com>
2022-11-10 23:36:16 -0800

    Returns a pointer to the allocated memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - returns a pointer to dynamic memory
+ * @b: size to be allocated
+ * Return: pointer for success, 98 for fail
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains c scripts writeen to test dynamic memmory allocation.
