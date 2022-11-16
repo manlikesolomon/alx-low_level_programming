d563b515a1da91c62590d1dcfee5ff8ca1e9fd06
Solomon <balogun97@icloud.com>
2022-11-15 16:53:54 -0800

    created struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - new structure dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Return: true
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+dog_t;
+
+#endif
